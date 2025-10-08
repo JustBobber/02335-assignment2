36ea2a3575c43b958f65119e0a43eda441323905
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2025-10-08 11:17:19 +0200

    make aq_send more readable

--- aq_tsafe.c
+++ aq_tsafe.c
@@ -5,13 +5,12 @@
  * @brief  Alarm queue skeleton implementation
  */
 
-
 #include <pthread.h>
 #include <string.h>
-
-#include "aq.h"
 #include "aq.h"
 #include "stdlib.h"
+
+#define SEND_SUCCESS 0
 
 typedef struct {
     void *val;
@@ -42,36 +41,42 @@ int aq_send(AlarmQueue aq, void *msg, MsgKind k) {
 
     // alarm message
     if (k == AQ_ALARM) {
+        // while an alarm already exists, we wait until a recvCondition tells us that a new
+        // message has been received. Then, we check again if an alarm already exists.
         while (queue->alarm != NULL) {
             pthread_cond_wait(&(queue->recvCondition), &(queue->lock));
         }
-        queue->alarm = msg;
-        pthread_cond_signal(&(queue->sendCondition));
-        pthread_mutex_unlock(&(queue->lock));
-        return 0;
+        queue->alarm = msg; // set alarm in queue
+        pthread_cond_signal(&(queue->sendCondition)); // signal that a message has been sent
+        pthread_mutex_unlock(&(queue->lock)); // release mutex
+        return SEND_SUCCESS;
     }
 
     // normal message
     if (k == AQ_NORMAL) {
+        // initialize new message
         NormalQueueMessage *new_msg = malloc(sizeof(NormalQueueMessage));
         new_msg->next = NULL;
+        new_msg->val = msg;
+
         if (queue->q_msg == NULL) {
+            // if message list head is null, set the new message as the head
             queue->q_msg = new_msg;
-            new_msg->val = msg; // add msg to queue
         } else {
+            // ... otherwise find the tail and append the new message
             NormalQueueMessage *current = queue->q_msg;
             while (current->next != NULL) {
                 current = current->next;
             }
             current->next = new_msg;
-            new_msg->val = msg; // add msg to queue
         }
-        pthread_cond_signal(&(queue->sendCondition));
-        pthread_mutex_unlock(&(queue->lock));
-        return 0;
+
+        pthread_cond_signal(&(queue->sendCondition)); // signal that a message has been sent
+        pthread_mutex_unlock(&(queue->lock)); // release mutex
+        return SEND_SUCCESS;
     }
 
-    // unknown message kind
+    // unknown message kind, release and return err code
     pthread_mutex_unlock(&(queue->lock));
     return AQ_NOT_IMPL;
 }
